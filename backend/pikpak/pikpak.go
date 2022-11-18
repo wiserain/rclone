wiserain/rclone

commit 8a2af3b32380c3776ec42d26dea0074189ad34fe

mod: pikpak: improve backend `pikpak` (#4)
* fix CleanUp() and MergeDirs() to use chunked batchaction
* new backend command `getid` `addurl` `decompress`
* add checks for validity of token on newFs()

backend/pikpak/pikpak.go
@@ -15,6 +15,8 @@ package pikpak
 
 // There are some cases with no downloadUrl for Open(). e.g. 0byte file
 
+// Trashed files are not restored to the original location when using `batchUntrash`
+
 // result of `rclone test info --all -vv --write-json remote.json remote:test`
 // stringNeedsEscaping = []rune{
 // 	' ', '!', '$', ''', '(', '*', '+', '.', '/', '0', '3', '4', '6', '8', '9', ':', '<', '>', '?', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z', '\"', '\\', '\a', '\b', '\f', '\n', '\r', '\t', '\u007f', '\u00a0', '\v', '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x0e', '\x0f', '\x10', '\x11', '\x12', '\x13', '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1a', '\x1b', '\x1c', '\x1d', '\x1e', '\x1f', '\xbf', '\xfe', '^', 'a', 'b', 'c', 'd', 'l', 'm', 'n', 'o', 'q', 'r', 'v', 'w', '|', '＼'
@@ -27,6 +29,22 @@ package pikpak
 // canReadUnnormalized   = true
 // canReadRenormalized   = false
 // canStream = false
+
+// ------------------------------------------------------------
+// FIXME
+// ------------------------------------------------------------
+
+// * Even after emptied trash, files are still visible with `--pikpak-trashed-only`. This goes aways after few days.
+
+// ------------------------------------------------------------
+// TODO
+// ------------------------------------------------------------
+
+// * List() with options starred-only and completed-only
+// * PublicLink() with more user-configurable options
+// * upload() with configurable chunk-size
+// * backend command: untrash
+// * api(event,task)
 
 import (
 	"bytes"
@@ -462,6 +480,16 @@ func newFs(ctx context.Context, name, path string, m configmap.Mapper) (*Fs, err
 		CanHaveEmptyDirectories: true, // can have empty directories
 	}).Fill(ctx, f)
 
+	// Check if current token is valid and re-authorize if necessary.
+	// It is somehow redundant since it is covered in shouldRetry().
+	// However it is a preemptive measure to avoid possible retries while validating a token.
+	if token, err := oauthutil.GetToken(name, m); err == nil && !token.Valid() {
+		fs.Debugf(f, "Token is invalid. Trying to get a new one using username/password...")
+		if err := f.doAuthorize(ctx); err != nil {
+			return nil, fserrors.FatalError(err)
+		}
+	}
+
 	return f, nil
 }
 
@@ -646,7 +674,7 @@ func (f *Fs) listAll(ctx context.Context, dirID, kind, trashed string, fn listAl
 	// Url Parameters
 	params := url.Values{}
 	params.Set("thumbnail_size", api.ThumbnaleSizeM)
-	params.Set("limit", strconv.Itoa(100))
+	params.Set("limit", strconv.Itoa(api.ListLimit))
 	params.Set("with_audit", strconv.FormatBool(true))
 	if parentId := parentIdForRequest(dirID); parentId != "" {
 		params.Set("parent_id", parentId)
@@ -925,6 +953,14 @@ func (f *Fs) CleanUp(ctx context.Context) (err error) {
 	var IDs []string
 	_, err = f.listAll(ctx, "*", "", "true", func(item *api.File) bool {
 		IDs = append(IDs, item.Id)
+		// API doesn't allow to delete a large number of objects at once, so doing it in chunked
+		if len(IDs) >= api.ListLimit {
+			if err = f.deleteObjects(ctx, IDs, false); err != nil {
+				return true
+			} else {
+				IDs = nil
+			}
+		}
 		return false
 	})
 	if err != nil {
@@ -1160,7 +1196,7 @@ func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object,
 
 // MergeDirs merges the contents of all the directories passed
 // in into the first one and rmdirs the other directories.
-func (f *Fs) MergeDirs(ctx context.Context, dirs []fs.Directory) error {
+func (f *Fs) MergeDirs(ctx context.Context, dirs []fs.Directory) (err error) {
 	if len(dirs) < 2 {
 		return nil
 	}
@@ -1171,16 +1207,24 @@ func (f *Fs) MergeDirs(ctx context.Context, dirs []fs.Directory) error {
 		// Moving objects in recycler throws following error:
 		// file_move_from_recycle_bin (9): "Moving in recycler is not allowed"
 		var IDs []string
-		_, err := f.listAll(ctx, srcDir.ID(), "", "false", func(item *api.File) bool {
+		_, err = f.listAll(ctx, srcDir.ID(), "", "false", func(item *api.File) bool {
 			fs.Infof(srcDir, "listing for merging %q", item.Name)
 			IDs = append(IDs, item.Id)
+			// API doesn't allow to move a large number of objects at once, so doing it in chunked
+			if len(IDs) >= api.ListLimit {
+				if err = f.moveObjects(ctx, IDs, dstDir.ID()); err != nil {
+					return true
+				} else {
+					IDs = nil
+				}
+			}
 			return false
 		})
 		if err != nil {
 			return fmt.Errorf("MergeDirs list failed on %v: %w", srcDir, err)
 		}
 		// move them into place
-		if err := f.moveObjects(ctx, IDs, dstDir.ID()); err != nil {
+		if err = f.moveObjects(ctx, IDs, dstDir.ID()); err != nil {
 			return fmt.Errorf("MergeDirs move failed in %v: %w", srcDir, err)
 		}
 	}
@@ -1190,6 +1234,14 @@ func (f *Fs) MergeDirs(ctx context.Context, dirs []fs.Directory) error {
 	for _, srcDir := range dirs[1:] {
 		fs.Infof(srcDir, "removing empty directory")
 		IDs = append(IDs, srcDir.ID())
+		// API doesn't allow to delete a large number of objects at once, so doing it in chunked
+		if len(IDs) >= api.ListLimit {
+			if err = f.deleteObjects(ctx, IDs, true); err != nil {
+				return err
+			} else {
+				IDs = nil
+			}
+		}
 	}
 	if err := f.deleteObjects(ctx, IDs, true); err != nil {
 		return fmt.Errorf("MergeDirs failed to rmdir: %w", err)
@@ -1326,7 +1378,120 @@ func (f *Fs) UserInfo(ctx context.Context) (userInfo map[string]string, err erro
 	}, nil
 }
 
-var commandHelp = []fs.CommandHelp{{}}
+// ------------------------------------------------------------
+
+// get an id of file or directory
+func (f *Fs) getID(ctx context.Context, path string) (id string, err error) {
+	if id, _ := parseRootID(path); len(id) > 6 {
+		info, err := f.getFile(ctx, id)
+		if err != nil {
+			return "", fmt.Errorf("couldn't find id: %w", err)
+		}
+		return info.Id, nil
+	}
+	path = strings.Trim(path, "/")
+	id, err = f.dirCache.FindDir(ctx, path, false)
+	if err != nil {
+		o, err := f.NewObject(ctx, path)
+		if err != nil {
+			return "", err
+		}
+		id = o.(fs.IDer).ID()
+	}
+	return id, nil
+}
+
+// add offline download task for url
+func (f *Fs) addUrl(ctx context.Context, url, path string) (*api.Task, error) {
+	req := api.RequestNewTask{
+		Kind:       api.KindOfFile,
+		UploadType: "UPLOAD_TYPE_URL",
+		Url: &api.Url{
+			Url: url,
+		},
+		FolderType: "DOWNLOAD",
+	}
+	if parentId, err := f.getID(ctx, path); err == nil {
+		req.ParentId = parentIdForRequest(parentId)
+		req.FolderType = ""
+	}
+	return f.requestNewTask(ctx, &req)
+}
+
+// decompress file/files in a directory of an ID
+func (f *Fs) decompressFiles(ctx context.Context, filename, id, password string, srcDelete bool) (err error) {
+	var files []*api.File
+	_, err = f.listAll(ctx, id, api.KindOfFile, "false", func(item *api.File) bool {
+		if item.MimeType == "application/zip" || item.MimeType == "application/x-7z-compressed" || item.MimeType == "application/x-rar-compressed" {
+			if filename == "" || filename == item.Name {
+				files = append(files, item)
+			}
+		}
+		return false
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't list files to decompress: %w", err)
+	}
+
+	for _, file := range files {
+		res, err := f.requestDecompress(ctx, file, password)
+		if err != nil {
+			fs.Errorf(f, "Unexpected error occurred while requesting decompress of %q: %w", file.Name, err)
+		} else if res.Status != "OK" {
+			fs.Errorf(f, "%q: %d files: %s", file.Name, res.FilesNum, res.Status)
+		} else {
+			fs.Infof(f, "%q: %d files: %s", file.Name, res.FilesNum, res.Status)
+			if srcDelete {
+				derr := f.deleteObjects(ctx, []string{file.Id}, f.opt.UseTrash)
+				if derr != nil {
+					fs.Errorf(f, "failed to delete %q: %w", file.Name, derr)
+				}
+			}
+		}
+	}
+	return
+}
+
+var commandHelp = []fs.CommandHelp{{
+	Name:  "getid",
+	Short: "Get IDs of files or directories",
+	Long: `This command is to obtain IDs of files or directories.
+
+Usage:
+
+    rclone backend getid pikpak:path {subpath}
+
+The 'path' should point to a directory not a file. Use an extra argument
+'subpath' to get an ID of a file located in 'pikpak:path'.
+`,
+}, {
+	Name:  "addurl",
+	Short: "Add offline download task for url",
+	Long: `This command adds offline download task for url.
+
+Usage:
+
+    rclone backend addurl pikpak:dirpath url
+
+Downloads will be stored in 'dirpath'. If 'dirpath' is invalid, 
+download will fallback to default 'My Pack' folder.
+`,
+}, {
+	Name:  "decompress",
+	Short: "Request decompress of file/files in a folder",
+	Long: `This command requests decompress of file/files in a folder.
+
+Usage:
+
+    rclone backend decompress pikpak:dirpath {filename} -o password=password
+	rclone backend decompress pikpak:dirpath {filename} -o delete-src-file
+
+An optional argument 'filename' can be specified for a file located in 
+'pikpak:dirpath'. You may want to pass '-o password=password' for a 
+password-protected files. Also, pass '-o delete-src-file' to delete 
+source files after decompression finished.
+`,
+}}
 
 // Command the backend to run a named command
 //
@@ -1338,8 +1503,39 @@ var commandHelp = []fs.CommandHelp{{}}
 // If it is a string or a []string it will be shown to the user
 // otherwise it will be JSON encoded and shown to the user like that
 func (f *Fs) Command(ctx context.Context, name string, arg []string, opt map[string]string) (out interface{}, err error) {
-	// TODO: untrash
-	return nil, nil
+	switch name {
+	case "getid":
+		path := ""
+		if len(arg) > 0 {
+			path = arg[0]
+		}
+		return f.getID(ctx, path)
+	case "addurl":
+		if len(arg) != 1 {
+			return nil, errors.New("need exactly 1 argument")
+		}
+		return f.addUrl(ctx, arg[0], "")
+	case "decompress":
+		filename := ""
+		if len(arg) > 0 {
+			filename = arg[0]
+		}
+		id, err := f.getID(ctx, "")
+		if err != nil {
+			return nil, fmt.Errorf("failed to get an ID of dirpath: %w", err)
+		}
+		password := ""
+		if pass, ok := opt["password"]; ok {
+			password = pass
+		}
+		_, srcDelete := opt["delete-src-file"]
+		return nil, f.decompressFiles(ctx, filename, id, password, srcDelete)
+	case "untrash":
+		// TODO: untrash
+		return nil, nil
+	default:
+		return nil, fs.ErrorCommandNotFound
+	}
 }
 
 // ------------------------------------------------------------

backend/pikpak/api/types.go
@@ -50,6 +50,7 @@ const (
 	PhaseTypeComplete = "PHASE_TYPE_COMPLETE"
 	PhaseTypeRunning  = "PHASE_TYPE_RUNNING"
 	PhaseTypeError    = "PHASE_TYPE_ERROR"
+	ListLimit         = 100
 )
 
 // ------------------------------------------------------------
@@ -294,6 +295,17 @@ type ResumableParams struct {
 	SecurityToken   string `json:"security_token,omitempty"`
 }
 
+type FileInArchive struct {
+	Index    int    `json:"index,omitempty"`
+	Filename string `json:"filename,omitempty"`
+	Filesize string `json:"filesize,omitempty"`
+	MimeType string `json:"mime_type,omitempty"`
+	Gcid     string `json:"gcid,omitempty"`
+	Kind     string `json:"kind,omitempty"`
+	IconLink string `json:"icon_link,omitempty"`
+	Path     string `json:"path,omitempty"`
+}
+
 // ------------------------------------------------------------
 
 type NewFile struct {
@@ -361,6 +373,14 @@ type UserProvider struct {
 	Name           string `json:"name,omitempty"` // username
 }
 
+type DecompressResult struct {
+	Status       string `json:"status,omitempty"` // "OK"
+	StatusText   string `json:"status_text,omitempty"`
+	TaskId       string `json:"task_id,omitempty"`   // same as File.Id
+	FilesNum     int    `json:"files_num,omitempty"` // number of files in archive
+	RedirectLink string `json:"redirect_link,omitempty"`
+}
+
 // ------------------------------------------------------------
 
 type RequestShare struct {
@@ -405,6 +425,14 @@ type RequestNewTask struct {
 	FolderType string `json:"folder_type,omitempty"` // "" if parent_id else "DOWNLOAD"
 }
 
+type RequestDecompress struct {
+	Gcid          string           `json:"gcid,omitempty"`     // same as File.Hash
+	Password      string           `json:"password,omitempty"` // ""
+	FileId        string           `json:"file_id,omitempty"`
+	Files         []*FileInArchive `json:"files,omitempty"` // can request selected files to be decompressed
+	DefaultParent bool             `json:"default_parent,omitempty"`
+}
+
 // ------------------------------------------------------------
 
 // NOT implemented YET
@@ -423,48 +451,20 @@ type VIP struct {
 }
 
 // POST https://api-drive.mypikpak.com/decompress/v1/list
-type RequestDecompressList struct {
-	Gcid     string `json:"gcid,omitempty"`
-	Path     string `json:"path,omitempty"`
-	Password string `json:"password,omitempty"`
+type RequestArchiveFileList struct {
+	Gcid     string `json:"gcid,omitempty"`     // same as api.File.Hash
+	Path     string `json:"path,omitempty"`     // "" by default
+	Password string `json:"password,omitempty"` // "" by default
 	FileId   string `json:"file_id,omitempty"`
 }
 
-type DecompressList struct {
-	Status      string            `json:"status,omitempty"` // "OK"
-	StatusText  string            `json:"status_text,omitempty"`
-	TaskId      string            `json:"task_id,omitempty"`
-	CurrentPath string            `json:"current_path,omitempty"`
-	Title       string            `json:"title,omitempty"`
-	FileSize    int64             `json:"file_size,omitempty"`
-	Gcid        string            `json:"gcid,omitempty"` // same sa File.Hash
-	Files       []*DecompressFile `json:"files,omitempty"`
-}
-
-type DecompressFile struct {
-	Index    int    `json:"index,omitempty"`
-	Filename string `json:"filename,omitempty"`
-	Filesize string `json:"filesize,omitempty"`
-	MimeType string `json:"mime_type,omitempty"`
-	Gcid     string `json:"gcid,omitempty"`
-	Kind     string `json:"kind,omitempty"`
-	IconLink string `json:"icon_link,omitempty"`
-	Path     string `json:"path,omitempty"`
-}
-
-// POST https://api-drive.mypikpak.com/decompress/v1/decompress
-type RequestDecompress struct {
-	Gcid          string        `json:"gcid,omitempty"`
-	Password      string        `json:"password,omitempty"`
-	FileId        string        `json:"file_id,omitempty"`
-	Files         []interface{} `json:"files,omitempty"`
-	DefaultParent bool          `json:"default_parent,omitempty"`
-}
-
-type Decompressed struct {
-	Status       string `json:"status,omitempty"` // "OK"
-	StatusText   string `json:"status_text,omitempty"`
-	TaskId       string `json:"task_id,omitempty"`   // same as File.Id
-	FilesNum     int    `json:"files_num,omitempty"` // number of files in archive
-	RedirectLink string `json:"redirect_link,omitempty"`
+type ArchiveFileList struct {
+	Status      string           `json:"status,omitempty"`       // "OK"
+	StatusText  string           `json:"status_text,omitempty"`  // ""
+	TaskId      string           `json:"task_id,omitempty"`      // ""
+	CurrentPath string           `json:"current_path,omitempty"` // ""
+	Title       string           `json:"title,omitempty"`
+	FileSize    int64            `json:"file_size,omitempty"`
+	Gcid        string           `json:"gcid,omitempty"` // same as File.Hash
+	Files       []*FileInArchive `json:"files,omitempty"`
 }

backend/pikpak/helper.go
@@ -20,6 +20,27 @@ const (
 	cachePrefix = "rclone-pikpak-sha1sum-"
 )
 
+// requestDecompress requests decompress of compressed files
+func (f *Fs) requestDecompress(ctx context.Context, file *api.File, password string) (info *api.DecompressResult, err error) {
+	req := &api.RequestDecompress{
+		Gcid:          file.Hash,
+		Password:      password,
+		FileId:        file.Id,
+		Files:         []*api.FileInArchive{},
+		DefaultParent: true,
+	}
+	opts := rest.Opts{
+		Method: "POST",
+		Path:   "/decompress/v1/decompress",
+	}
+	var resp *http.Response
+	err = f.pacer.Call(func() (bool, error) {
+		resp, err = f.srv.CallJSON(ctx, &opts, &req, &info)
+		return f.shouldRetry(ctx, resp, err)
+	})
+	return
+}
+
 // getUserInfo gets UserInfo from API
 func (f *Fs) getUserInfo(ctx context.Context) (info *api.User, err error) {
 	opts := rest.Opts{
@@ -55,6 +76,24 @@ func (f *Fs) requestBatchAction(ctx context.Context, action string, req *api.Req
 		return fmt.Errorf("batch action %q failed: %w", action, err)
 	}
 	return nil
+}
+
+// requestNewTask requests a new api.NewTask and returns api.Task
+func (f *Fs) requestNewTask(ctx context.Context, req *api.RequestNewTask) (info *api.Task, err error) {
+	opts := rest.Opts{
+		Method: "POST",
+		Path:   "/drive/v1/files",
+	}
+	var newTask api.NewTask
+	var resp *http.Response
+	err = f.pacer.Call(func() (bool, error) {
+		resp, err = f.srv.CallJSON(ctx, &opts, &req, &newTask)
+		return f.shouldRetry(ctx, resp, err)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return newTask.Task, nil
 }
 
 // requestNewFile requests a new api.NewFile and returns api.File

commit 7c9ec93d6f891fdcf7e32852be59728971657fd8

fix lint error introduced in 8a2af3b32380c3776ec42d26dea0074189ad34fe
* `decompress` returns more details

backend/pikpak/pikpak.go
@@ -1411,45 +1411,63 @@ func (f *Fs) addUrl(ctx context.Context, url, path string) (*api.Task, error) {
 		},
 		FolderType: "DOWNLOAD",
 	}
-	if parentId, err := f.getID(ctx, path); err == nil {
+	if parentId, err := f.dirCache.FindDir(ctx, path, false); err == nil {
 		req.ParentId = parentIdForRequest(parentId)
 		req.FolderType = ""
 	}
 	return f.requestNewTask(ctx, &req)
 }
 
+type decompressDirResult struct {
+	Decompressed  int
+	SourceDeleted int
+	Errors        int
+}
+
+func (r decompressDirResult) Error() string {
+	return fmt.Sprintf("%d error(s) while decompressing - see log", r.Errors)
+}
+
 // decompress file/files in a directory of an ID
-func (f *Fs) decompressFiles(ctx context.Context, filename, id, password string, srcDelete bool) (err error) {
-	var files []*api.File
+func (f *Fs) decompressDir(ctx context.Context, filename, id, password string, srcDelete bool) (r decompressDirResult, err error) {
 	_, err = f.listAll(ctx, id, api.KindOfFile, "false", func(item *api.File) bool {
 		if item.MimeType == "application/zip" || item.MimeType == "application/x-7z-compressed" || item.MimeType == "application/x-rar-compressed" {
 			if filename == "" || filename == item.Name {
-				files = append(files, item)
+				res, err := f.requestDecompress(ctx, item, password)
+				if err != nil {
+					err = fmt.Errorf("unexpected error while requesting decompress of %q: %w", item.Name, err)
+					r.Errors++
+					fs.Errorf(f, "%v", err)
+				} else if res.Status != "OK" {
+					r.Errors++
+					fs.Errorf(f, "%q: %d files: %s", item.Name, res.FilesNum, res.Status)
+				} else {
+					r.Decompressed++
+					fs.Infof(f, "%q: %d files: %s", item.Name, res.FilesNum, res.Status)
+					if srcDelete {
+						derr := f.deleteObjects(ctx, []string{item.Id}, f.opt.UseTrash)
+						if derr != nil {
+							derr = fmt.Errorf("failed to delete %q: %w", item.Name, derr)
+							r.Errors++
+							fs.Errorf(f, "%v", derr)
+						} else {
+							r.SourceDeleted++
+						}
+					}
+				}
 			}
 		}
 		return false
 	})
 	if err != nil {
-		return fmt.Errorf("couldn't list files to decompress: %w", err)
+		err = fmt.Errorf("couldn't list files to decompress: %w", err)
+		r.Errors++
+		fs.Errorf(f, "%v", err)
 	}
-
-	for _, file := range files {
-		res, err := f.requestDecompress(ctx, file, password)
-		if err != nil {
-			fs.Errorf(f, "Unexpected error occurred while requesting decompress of %q: %w", file.Name, err)
-		} else if res.Status != "OK" {
-			fs.Errorf(f, "%q: %d files: %s", file.Name, res.FilesNum, res.Status)
-		} else {
-			fs.Infof(f, "%q: %d files: %s", file.Name, res.FilesNum, res.Status)
-			if srcDelete {
-				derr := f.deleteObjects(ctx, []string{file.Id}, f.opt.UseTrash)
-				if derr != nil {
-					fs.Errorf(f, "failed to delete %q: %w", file.Name, derr)
-				}
-			}
-		}
+	if r.Errors != 0 {
+		return r, r
 	}
-	return
+	return r, nil
 }
 
 var commandHelp = []fs.CommandHelp{{
@@ -1520,7 +1538,7 @@ func (f *Fs) Command(ctx context.Context, name string, arg []string, opt map[str
 		if len(arg) > 0 {
 			filename = arg[0]
 		}
-		id, err := f.getID(ctx, "")
+		id, err := f.dirCache.FindDir(ctx, "", false)
 		if err != nil {
 			return nil, fmt.Errorf("failed to get an ID of dirpath: %w", err)
 		}
@@ -1529,7 +1547,7 @@ func (f *Fs) Command(ctx context.Context, name string, arg []string, opt map[str
 			password = pass
 		}
 		_, srcDelete := opt["delete-src-file"]
-		return nil, f.decompressFiles(ctx, filename, id, password, srcDelete)
+		return f.decompressDir(ctx, filename, id, password, srcDelete)
 	case "untrash":
 		// TODO: untrash
 		return nil, nil