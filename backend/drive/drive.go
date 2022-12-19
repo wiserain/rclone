wiserain/rclone

commit fb41be4506f33f9ad7a98c8e203b9d88487dd3e9

mod: fix issue not rotating sa when uploading large files with `--drive-service-account-per-file`

backend/drive/drive.go
@@ -2417,17 +2417,17 @@ func (f *Fs) PutUnchecked(ctx context.Context, in io.Reader, src fs.ObjectInfo, 
 		createInfo.MimeType = fs.MimeTypeFromName(remote)
 	}
 
+	// mod
+	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
+		f.changeSAmu.Lock()
+		defer f.changeSAmu.Unlock()
+		f.changeServiceAccount(ctx)
+	}
 	var info *drive.File
 	if size >= 0 && size < int64(f.opt.UploadCutoff) {
 		// Make the API request to upload metadata and file data.
 		// Don't retry, return a retry error instead
 		err = f.pacer.CallNoRetry(func() (bool, error) {
-			// mod
-			if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-				f.changeSAmu.Lock()
-				defer f.changeSAmu.Unlock()
-				f.changeServiceAccount(ctx)
-			}
 			info, err = f.svc.Files.Create(createInfo).
 				Media(in, googleapi.ContentType(srcMimeType), googleapi.ChunkSize(0)).
 				Fields(partialFields).
@@ -3524,8 +3524,8 @@ Result:
 `,
 }, { // mod
 	Name:  "getid",
-	Short: "Get IDs of files or directories",
-	Long: `This command is to obtain IDs of files or directories.
+	Short: "Get an ID of a file or directory",
+	Long: `This command is to obtain an ID of a file or directory.
 
 Usage:
 

backend/pikpak/pikpak.go
@@ -1473,8 +1473,8 @@ func (f *Fs) decompressDir(ctx context.Context, filename, id, password string, s
 
 var commandHelp = []fs.CommandHelp{{
 	Name:  "getid",
-	Short: "Get IDs of files or directories",
-	Long: `This command is to obtain IDs of files or directories.
+	Short: "Get an ID of a file or directory",
+	Long: `This command is to obtain an ID of a file or directory.
 
 Usage:
 
@@ -1497,13 +1497,13 @@ download will fallback to default 'My Pack' folder.
 `,
 }, {
 	Name:  "decompress",
-	Short: "Request decompress of file/files in a folder",
+	Short: "Request decompress of a file/files in a folder",
 	Long: `This command requests decompress of file/files in a folder.
 
 Usage:
 
     rclone backend decompress pikpak:dirpath {filename} -o password=password
-	rclone backend decompress pikpak:dirpath {filename} -o delete-src-file
+    rclone backend decompress pikpak:dirpath {filename} -o delete-src-file
 
 An optional argument 'filename' can be specified for a file located in 
 'pikpak:dirpath'. You may want to pass '-o password=password' for a 

commit d64f74e6deab38a6cb25fa3e7acd8c0c247f9ba8

mod: drive: fix transfer lock issue introduced in fb41be4506f33f9ad7a98c8e203b9d88487dd3e9
Also improve logic with `changeServiceAccount()` and `--drive-service-account-per-file`

backend/drive/drive.go
@@ -817,8 +817,6 @@ func (f *Fs) shouldRetry(ctx context.Context, err error) (bool, error) {
 			if reason == "rateLimitExceeded" || reason == "userRateLimitExceeded" || (reason == "dailyLimitExceededUnreg" || strings.HasPrefix(message, "Daily Limit")) {
 				// mod - try changing service account
 				if f.changeSAenabled {
-					f.changeSAmu.Lock()
-					defer f.changeSAmu.Unlock()
 					if saerr := f.changeServiceAccount(ctx); saerr == nil {
 						return true, err
 					}
@@ -2418,8 +2416,6 @@ func (f *Fs) PutUnchecked(ctx context.Context, in io.Reader, src fs.ObjectInfo, 
 
 	// mod
 	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-		f.changeSAmu.Lock()
-		defer f.changeSAmu.Unlock()
 		f.changeServiceAccount(ctx)
 	}
 	var info *drive.File
@@ -2663,14 +2659,12 @@ func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object,
 		id = actualID(srcObj.id)
 	}
 
+	// mod
+	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
+		f.changeServiceAccount(ctx)
+	}
 	var info *drive.File
 	err = f.pacer.Call(func() (bool, error) {
-		// mod
-		if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-			f.changeSAmu.Lock()
-			defer f.changeSAmu.Unlock()
-			f.changeServiceAccount(ctx)
-		}
 		copy := f.svc.Files.Copy(id, createInfo).
 			Fields(partialFields).
 			SupportsAllDrives(true).

backend/drive/mod.go
@@ -172,6 +172,8 @@ func (f *Fs) changeServiceAccount(ctx context.Context) (err error) {
 		fs.Debugf(nil, "retrying with the same service account")
 		return nil
 	}
+	f.changeSAmu.Lock()
+	defer f.changeSAmu.Unlock()
 	// reloading SA
 	if len(f.changeSApool.SAs) < 1 {
 		if err := f.changeSApool.LoadSA(); err != nil {
@@ -208,9 +210,9 @@ func (f *Fs) changeServiceAccount(ctx context.Context) (err error) {
 	if err == nil {
 		f.changeSAtime = time.Now()
 		f.pacer = fs.NewPacer(ctx, pacer.NewGoogleDrive(pacer.MinSleep(f.opt.PacerMinSleep), pacer.Burst(f.opt.PacerBurst)))
-		svcAcc := "a service account credential"
+		svcAcc := "service account credential"
 		if sa[0].ServiceAccountFile != "" {
-			svcAcc = fmt.Sprintf("a service account file \"%s\"", filepath.Base(sa[0].ServiceAccountFile))
+			svcAcc = fmt.Sprintf("service account file \"%s\"", filepath.Base(sa[0].ServiceAccountFile))
 		}
 		if sa[0].Impersonate != "" {
 			fs.Debugf(nil, "Now working with %s as %q", svcAcc, sa[0].Impersonate)