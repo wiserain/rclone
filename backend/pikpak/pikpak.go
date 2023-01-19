675b58fef260f96eea01057ece0db0ce9a72e87c
wiserain <mail275@gmail.com>
2023-01-19 11:42:54 +0900

    remove hard-coded useragent

--- backend/pikpak/pikpak.go
+++ backend/pikpak/pikpak.go
@@ -77,7 +77,6 @@ const (
 	maxSleep           = 2 * time.Second
 	decayConstant      = 2 // bigger for slower decay, exponential
 	rootURL            = "https://api-drive.mypikpak.com"
-	userAgent          = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.35"
 )
 
 // Globals
@@ -461,8 +460,6 @@ func newFs(ctx context.Context, name, path string, m configmap.Mapper) (*Fs, err
 	// multipart copy doesn't seem to be allowed when opening an object for download
 	// temporary workaround by forcing `--multi-thread-streams=1`
 	ci.MultiThreadStreams = 1
-	// override user-agent
-	ci.UserAgent = userAgent
 
 	f := &Fs{
 		name:    name,
