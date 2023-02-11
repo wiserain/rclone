3542129f1da549b639e9fe30bffb7872aa8eca1d
wiserain <mail275@gmail.com>
2023-02-11 23:15:20 +0900

    remove `parseRootID()`

--- backend/pikpak/pikpak.go
+++ backend/pikpak/pikpak.go
@@ -36,7 +36,6 @@ import (
 	"net/url"
 	"path"
 	"reflect"
-	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -286,28 +285,6 @@ func (f *Fs) Hashes() hash.Set {
 // parsePath parses a remote path
 func parsePath(path string) (root string) {
 	root = strings.Trim(path, "/")
-	return
-}
-
-// parseRootID parses RootID from path
-func parseRootID(s string) (rootID string, err error) {
-	re := regexp.MustCompile(`\{([^}]{5,})\}`)
-	m := re.FindStringSubmatch(s)
-	if m == nil {
-		return "", fmt.Errorf("%s does not contain a valid id", s)
-	}
-	rootID = m[1]
-
-	if strings.HasPrefix(rootID, "http") {
-		// https://mypikpak.com/drive/all/{ID}
-		// https://mypikpak.com/drive/recent/{ID}
-		// https://mypikpak.com/s/{ID}
-		re := regexp.MustCompile(`\/drive\/(all|recent)(\/([A-Za-z0-9_-]{6,}))+\/?`)
-		if m := re.FindStringSubmatch(rootID); m != nil {
-			rootID = m[len(m)-1]
-			return
-		}
-	}
 	return
 }
 
@@ -1367,13 +1344,6 @@ func (f *Fs) UserInfo(ctx context.Context) (userInfo map[string]string, err erro
 
 // get an id of file or directory
 func (f *Fs) getID(ctx context.Context, path string) (id string, err error) {
-	if id, _ := parseRootID(path); len(id) > 6 {
-		info, err := f.getFile(ctx, id)
-		if err != nil {
-			return "", fmt.Errorf("no such object with id %q: %w", id, err)
-		}
-		return info.Id, nil
-	}
 	path = strings.Trim(path, "/")
 	id, err = f.dirCache.FindDir(ctx, path, false)
 	if err != nil {
