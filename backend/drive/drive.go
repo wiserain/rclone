wiserain/rclone

commit 425123f2c104fd5de2eab90652f924714ce92d7c

mod: drive: fix lint errors reported by golangci-lint

backend/drive/drive.go
@@ -2660,7 +2660,9 @@ func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object,
 
 	// mod
 	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-		f.changeServiceAccount(ctx)
+		if saerr := f.changeServiceAccount(ctx); saerr == nil {
+			return nil, saerr
+		}
 	}
 	var info *drive.File
 	err = f.pacer.Call(func() (bool, error) {

backend/drive/upload.go
@@ -53,7 +53,9 @@ type resumableUpload struct {
 func (f *Fs) Upload(ctx context.Context, in io.Reader, size int64, contentType, fileID, remote string, info *drive.File) (*drive.File, error) {
 	// mod
 	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-		f.changeServiceAccount(ctx)
+		if saerr := f.changeServiceAccount(ctx); saerr == nil {
+			return nil, saerr
+		}
 	}
 	params := url.Values{
 		"alt":        {"json"},

commit 81cbfa93da303c539530dfac0efce7c340a4d96a

mod: drive: fix error with `--drive-service-account-per-file`
which was introduced in 425123f2c104fd5de2eab90652f924714ce92d7c

backend/drive/drive.go
@@ -2660,9 +2660,7 @@ func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object,
 
 	// mod
 	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-		if saerr := f.changeServiceAccount(ctx); saerr == nil {
-			return nil, saerr
-		}
+		_ = f.changeServiceAccount(ctx) // ignore error
 	}
 	var info *drive.File
 	err = f.pacer.Call(func() (bool, error) {

backend/drive/upload.go
@@ -53,9 +53,7 @@ type resumableUpload struct {
 func (f *Fs) Upload(ctx context.Context, in io.Reader, size int64, contentType, fileID, remote string, info *drive.File) (*drive.File, error) {
 	// mod
 	if f.changeSAenabled && f.opt.ServiceAccountPerFile {
-		if saerr := f.changeServiceAccount(ctx); saerr == nil {
-			return nil, saerr
-		}
+		_ = f.changeServiceAccount(ctx) // ignore error
 	}
 	params := url.Values{
 		"alt":        {"json"},
@@ -95,8 +93,11 @@ func (f *Fs) Upload(ctx context.Context, in io.Reader, size int64, contentType, 
 		if size >= 0 {
 			req.Header.Set("X-Upload-Content-Length", fmt.Sprintf("%v", size))
 		}
+		fmt.Println("before request upload")
 		res, err = f.client.Do(req)
+		fmt.Println("after request upload")
 		if err == nil {
+			fmt.Println("error nil")
 			defer googleapi.CloseBody(res)
 			err = googleapi.CheckResponse(res)
 		}