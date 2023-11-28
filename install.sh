6b704001b7db5bd8a83d270080dd08a33a5126be
wiserain <mail275@gmail.com>
2023-11-28 17:15:45 +0900

    install.sh: fix issues with cleanup tmp_dir (#8)
    * use trap for cleaning up `tmp_dir` instead of manual `rm -rf` at the end of script
    * apply shellcheck
    * stop redirecting to file `errors`
    * improve post installation checks
    * fix indent
    * move creating `tmp_dir` right before starting actual installation
    * remove trailing spaces

--- install.sh
+++ install.sh
@@ -15,43 +15,43 @@ unzip_tools_list=('unzip' '7z' 'busybox')
 usage() { echo "Usage: sudo -v ; curl https://raw.githubusercontent.com/wiserain/rclone/mod/install.sh | sudo bash" 1>&2; exit 1; }
 
 if [ -n "$1" ]; then
-    tag_name="$1"
+  tag_name="$1"
 else
-    tag_name=$(curl -fsS https://api.github.com/repos/wiserain/rclone/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+  tag_name=$(curl -fsS https://api.github.com/repos/wiserain/rclone/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+fi
+
+
+#make sure unzip tool is available and choose one to work with
+set +e
+for tool in "${unzip_tools_list[@]}"; do
+  if command -v "$tool" >/dev/null 2>&1; then
+    unzip_tool="$tool"
+    break
+  fi
+done
+set -e
+
+# exit if no unzip tools available
+if [ -z "$unzip_tool" ]; then
+  printf "\nNone of the supported tools for extracting zip archives (%s) were found." "${unzip_tools_list[*]}"
+  printf "Please install one of them and try again.\n\n"
+  exit 4
 fi
 
 
 #create tmp directory and move to it with macOS compatibility fallback
 tmp_dir=$(mktemp -d 2>/dev/null || mktemp -d -t 'rclone-install.XXXXXXXXXX')
+trap 'rm -rf "$tmp_dir"' EXIT
 cd "$tmp_dir"
-
-
-#make sure unzip tool is available and choose one to work with
-set +e
-for tool in ${unzip_tools_list[*]}; do
-    trash=$(hash "$tool" 2>>errors)
-    if [ "$?" -eq 0 ]; then
-        unzip_tool="$tool"
-        break
-    fi
-done  
-set -e
-
-# exit if no unzip tools available
-if [ -z "$unzip_tool" ]; then
-    printf "\nNone of the supported tools for extracting zip archives (${unzip_tools_list[*]}) were found. "
-    printf "Please install one of them and try again.\n\n"
-    exit 4
-fi
 
 # Make sure we don't create a root owned .config/rclone directory #2127
 export XDG_CONFIG_HOME=config
 
 #check installed version of rclone to determine if update is necessary
-version=$(rclone --version 2>>errors | head -n 1 | cut -d ' ' -f 2)
+version=$(rclone --version 2>/dev/null | head -n1 | cut -d' ' -f2)
 if [ "$version" = "$tag_name" ]; then
-    printf "\nThe latest version of rclone mod ${version} is already installed.\n\n"
-    exit 3
+  printf "\nThe latest version of rclone mod %s is already installed.\n\n" "${version}"
+  exit 3
 fi
 
 
@@ -61,7 +61,7 @@ OS="$(uname)"
 case $OS in
   Linux)
     OS='linux'
-    echo $PREFIX | grep -qo "com.termux" && \
+    echo "$PREFIX" | grep -qo "com.termux" && \
       { OS="termux"; unzip_tool="deb"; }
     ;;
   FreeBSD)
@@ -72,7 +72,7 @@ case $OS in
     ;;
   OpenBSD)
     OS='openbsd'
-    ;;  
+    ;;
   Darwin)
     OS='osx'
     binTgtDir=/usr/local/bin
@@ -139,7 +139,7 @@ case "$unzip_tool" in
     mkdir -p "$unzip_dir/empty"
     ;;
 esac
-    
+
 cd $unzip_dir/*
 
 
@@ -155,7 +155,7 @@ case "$OS" in
     #manual
     if ! [ -x "$(command -v mandb)" ]; then
         echo 'mandb not found. The rclone man docs will not be installed.'
-    else 
+    else
         mkdir -p /usr/local/share/man/man1
         cp rclone.1 /usr/local/share/man/man1/
         mandb
@@ -173,30 +173,28 @@ case "$OS" in
     ;;
   'osx')
     #binary
-    mkdir -m 0555 -p ${binTgtDir}
+    mkdir -p ${binTgtDir} && chmod -m 0555 ${binTgtDir}
     cp rclone ${binTgtDir}/rclone.new
     mv ${binTgtDir}/rclone.new ${binTgtDir}/rclone
     chmod a=x ${binTgtDir}/rclone
     #manual
-    mkdir -m 0555 -p ${man1TgtDir}
-    cp rclone.1 ${man1TgtDir}    
-    chmod a=r ${man1TgtDir}/rclone.1    
+    mkdir -p ${man1TgtDir} && chmod -m 0555 ${man1TgtDir}
+    cp rclone.1 ${man1TgtDir}
+    chmod a=r ${man1TgtDir}/rclone.1
     ;;
   'termux')
     cd ../..
-    dpkg -i $rclone_zip
+    dpkg -i "$rclone_zip"
     ;;
   *)
     echo 'OS not supported'
     exit 2
 esac
 
-#cleanup
-rm -rf "$tmp_dir"
+# checking binary available
+command -v rclone >/dev/null 2>&1 || \
+  { printf "\nERROR: rclone seems not installed.\n\n"; exit 1; }
 
-#update version variable post install
-version=$(rclone --version 2>>errors | head -n 1)
-
-printf "\n${version} has successfully installed."
+printf "\n%s has successfully installed." "$(rclone --version | head -n1)"
 printf '\nNow run "rclone config" for setup. Check https://rclone.org/docs/ for more details.\n\n'
 exit 0
