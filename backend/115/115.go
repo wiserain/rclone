cdcf607a3d091aec348be71da6c2031713a0f427
wiserain <mail275@gmail.com>
2024-08-30 09:20:01 +0900

    115: increase default min sleep for pacer to 250ms (reduces tps to 4)

--- backend/115/115.go
+++ backend/115/115.go
@@ -51,7 +51,7 @@ const (
 	appVer           = "2.0.3.6"
 	defaultUserAgent = "Mozilla/5.0 115Desktop/" + appVer
 
-	defaultMinSleep = fs.Duration(150 * time.Millisecond) // 6.67 transactions per second
+	defaultMinSleep = fs.Duration(250 * time.Millisecond) // 4 transactions per second
 	maxSleep        = 2 * time.Second
 	decayConstant   = 2 // bigger for slower decay, exponential
 
