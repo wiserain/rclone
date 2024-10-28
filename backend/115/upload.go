wiserain/rclone

commit fb6c6553299cc5730a8f631760e4af966fc3c09a

115: update to use OSS SDK v2

backend/115/upload.go
@@ -5,7 +5,6 @@ import (
 	"context"
 	"crypto/md5"
 	"crypto/sha1"
-	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
@@ -18,7 +17,8 @@ import (
 	"strings"
 	"time"
 
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
 	"github.com/rclone/rclone/backend/115/api"
 	"github.com/rclone/rclone/backend/115/cipher"
 	"github.com/rclone/rclone/fs"
@@ -32,7 +32,7 @@ import (
 const (
 	cachePrefix  = "rclone-115-sha1sum-"
 	md5Salt      = "Qclm8MGWUv59TnrR0XPg"
-	OSSEndpoint  = "http://oss-cn-shenzhen.aliyuncs.com" // https://uplb.115.com/3.0/getuploadinfo.php
+	OSSRegion    = "cn-shenzhen" // https://uplb.115.com/3.0/getuploadinfo.php
 	OSSUserAgent = "aliyun-sdk-android/2.9.1"
 )
 
@@ -232,49 +232,29 @@ func (f *Fs) getOSSToken(ctx context.Context) (info *api.OSSToken, err error) {
 	return
 }
 
-func ossOpts(ossOptions []oss.Option, options ...fs.OpenOption) []oss.Option {
-	opts := ossOptions
-
-	// Apply upload options
-	for _, option := range options {
-		key, value := option.Header()
-		lowerKey := strings.ToLower(key)
-		switch lowerKey {
-		case "":
-			// ignore
-		case "cache-control":
-			opts = append(opts, oss.CacheControl(value))
-		case "content-disposition":
-			opts = append(opts, oss.ContentDisposition(value))
-		case "content-encoding":
-			opts = append(opts, oss.ContentEncoding(value))
+func (f *Fs) newOSSClient() (client *oss.Client) {
+	customerProvider := credentials.CredentialsProviderFunc(func(ctx context.Context) (credentials.Credentials, error) {
+		t, err := f.getOSSToken(ctx)
+		if err != nil {
+			return credentials.Credentials{}, err
 		}
-	}
-	return opts
-}
+		return credentials.Credentials{
+			AccessKeyID:     t.AccessKeyID,
+			AccessKeySecret: t.AccessKeySecret,
+			SecurityToken:   t.SecurityToken,
+			Expires:         &t.Expiration}, nil
+	})
+	provider := credentials.CredentialsProviderFunc(func(ctx context.Context) (credentials.Credentials, error) {
+		return customerProvider.GetCredentials(ctx)
+	})
 
-func (f *Fs) newOSSBucket(ctx context.Context, ui *api.UploadInitInfo) (bucket *oss.Bucket, callback []oss.Option, token *api.OSSToken, err error) {
-	token, err = f.getOSSToken(ctx)
-	if err != nil {
-		err = fmt.Errorf("failed to get OSS token: %w", err)
-		return
-	}
-	client, err := oss.New(OSSEndpoint, token.AccessKeyID, token.AccessKeySecret, []oss.ClientOption{
-		oss.HTTPClient(f.client),
-		oss.SecurityToken(token.SecurityToken),
-		oss.UserAgent(OSSUserAgent),
-	}...)
-	if err != nil {
-		err = fmt.Errorf("failed to create a new OSS client: %w", err)
-		return
-	}
-	if bucket, err = client.Bucket(ui.Bucket); err == nil {
-		callback = []oss.Option{
-			oss.Callback(base64.StdEncoding.EncodeToString([]byte(ui.Callback.Callback))),
-			oss.CallbackVar(base64.StdEncoding.EncodeToString([]byte(ui.Callback.CallbackVar))),
-		}
-	}
-	return
+	cfg := oss.LoadDefaultConfig().
+		WithCredentialsProvider(provider).
+		WithRegion(OSSRegion).
+		WithUserAgent(OSSUserAgent).
+		WithHttpClient(f.client)
+
+	return oss.NewClient(cfg)
 }
 
 // unWrapObjectInfo returns the underlying Object unwrapped as much as
@@ -403,9 +383,32 @@ func (f *Fs) upload(ctx context.Context, in io.Reader, src fs.ObjectInfo, remote
 	}
 
 	// upload singlepart
-	bucket, callback, _, err := f.newOSSBucket(ctx, ui)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create a new OSS bucket: %w", err)
+	client := f.newOSSClient()
+	req := &oss.PutObjectRequest{
+		Bucket:      oss.Ptr(ui.Bucket),
+		Key:         oss.Ptr(ui.Object),
+		Body:        in,
+		Callback:    oss.Ptr(ui.GetCallback()),
+		CallbackVar: oss.Ptr(ui.GetCallbackVar()),
 	}
-	return o, bucket.PutObject(ui.Object, in, ossOpts(callback, options...)...)
+	// Apply upload options
+	for _, option := range options {
+		key, value := option.Header()
+		lowerKey := strings.ToLower(key)
+		switch lowerKey {
+		case "":
+			// ignore
+		case "cache-control":
+			req.CacheControl = oss.Ptr(value)
+		case "content-disposition":
+			req.ContentDisposition = oss.Ptr(value)
+		case "content-encoding":
+			req.ContentEncoding = oss.Ptr(value)
+		case "content-type":
+			req.ContentType = oss.Ptr(value)
+		}
+	}
+
+	_, err = client.PutObject(ctx, req)
+	return o, err
 }

backend/115/api/types.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -346,6 +347,14 @@ type UploadInitInfo struct {
 	// New fields in upload v4.0
 	SignKey   string `json:"sign_key"`
 	SignCheck string `json:"sign_check"`
+}
+
+func (ui *UploadInitInfo) GetCallback() string {
+	return base64.StdEncoding.EncodeToString([]byte(ui.Callback.Callback))
+}
+
+func (ui *UploadInitInfo) GetCallbackVar() string {
+	return base64.StdEncoding.EncodeToString([]byte(ui.Callback.CallbackVar))
 }
 
 type OSSToken struct {

backend/115/multipart.go
@@ -5,10 +5,11 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"strings"
 	"sync"
 	"time"
 
-	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
 	"github.com/rclone/rclone/backend/115/api"
 	"github.com/rclone/rclone/fs"
 	"github.com/rclone/rclone/fs/accounting"
@@ -152,14 +153,10 @@ type ossChunkWriter struct {
 	in            io.Reader
 	mu            sync.Mutex
 	uploadedParts []oss.UploadPart
-	bucket        *oss.Bucket
-	callback      []oss.Option
-	imur          oss.InitiateMultipartUploadResult
-	// token
-	token       *api.OSSToken
-	done        chan any
-	shutdown    sync.Once
-	expiryTimer *time.Timer // signals whenever the token expires
+	client        *oss.Client
+	callback      string
+	callbackVar   string
+	imur          *oss.InitiateMultipartUploadResult
 }
 
 func (f *Fs) newChunkWriter(ctx context.Context, remote string, src fs.ObjectInfo, ui *api.UploadInitInfo, in io.Reader, options ...fs.OpenOption) (w *ossChunkWriter, err error) {
@@ -198,22 +195,39 @@ func (f *Fs) newChunkWriter(ctx context.Context, remote string, src fs.ObjectInf
 		f:         f,
 		o:         o,
 		in:        in,
+		client:    f.newOSSClient(),
 	}
 
-	w.bucket, w.callback, w.token, err = f.newOSSBucket(ctx, ui)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create a new OSS bucket: %w", err)
+	req := &oss.InitiateMultipartUploadRequest{
+		Bucket: oss.Ptr(ui.Bucket),
+		Key:    oss.Ptr(ui.Object),
 	}
-
+	// Apply upload options
+	for _, option := range options {
+		key, value := option.Header()
+		lowerKey := strings.ToLower(key)
+		switch lowerKey {
+		case "":
+			// ignore
+		case "cache-control":
+			req.CacheControl = oss.Ptr(value)
+		case "content-disposition":
+			req.ContentDisposition = oss.Ptr(value)
+		case "content-encoding":
+			req.ContentEncoding = oss.Ptr(value)
+		case "content-type":
+			req.ContentType = oss.Ptr(value)
+		}
+	}
 	err = w.f.pacer.Call(func() (bool, error) {
-		w.imur, err = w.bucket.InitiateMultipartUpload(ui.Object, ossOpts(nil, options...)...)
+		w.imur, err = w.client.InitiateMultipartUpload(ctx, req)
 		return w.shouldRetry(ctx, err)
 	})
 	if err != nil {
 		return nil, fmt.Errorf("create multipart upload failed: %w", err)
 	}
-	w.startRenew()
-	fs.Debugf(w.o, "multipart upload: %q initiated", w.imur.UploadID)
+	w.callback, w.callbackVar = ui.GetCallback(), ui.GetCallbackVar()
+	fs.Debugf(w.o, "multipart upload: %q initiated", *w.imur.UploadId)
 	return
 }
 
@@ -258,7 +272,7 @@ func (w *ossChunkWriter) WriteChunk(ctx context.Context, chunkNumber int, reader
 	}
 
 	ossPartNumber := chunkNumber + 1
-	var uout oss.UploadPart
+	var res *oss.UploadPartResult
 	err = w.f.pacer.Call(func() (bool, error) {
 		// Discover the size by seeking to the end
 		currentChunkSize, err = reader.Seek(0, io.SeekEnd)
@@ -270,7 +284,13 @@ func (w *ossChunkWriter) WriteChunk(ctx context.Context, chunkNumber int, reader
 		if err != nil {
 			return false, err
 		}
-		uout, err = w.bucket.UploadPart(w.imur, reader, currentChunkSize, ossPartNumber)
+		res, err = w.client.UploadPart(ctx, &oss.UploadPartRequest{
+			Bucket:     w.imur.Bucket,
+			Key:        w.imur.Key,
+			UploadId:   w.imur.UploadId,
+			PartNumber: int32(ossPartNumber),
+			Body:       reader,
+		})
 		if err != nil {
 			if chunkNumber <= 8 {
 				return w.shouldRetry(ctx, err)
@@ -284,7 +304,10 @@ func (w *ossChunkWriter) WriteChunk(ctx context.Context, chunkNumber int, reader
 		return -1, fmt.Errorf("failed to upload chunk %d with %v bytes: %w", ossPartNumber, currentChunkSize, err)
 	}
 
-	w.addCompletedPart(uout)
+	w.addCompletedPart(oss.UploadPart{
+		PartNumber: int32(ossPartNumber),
+		ETag:       res.ETag,
+	})
 
 	fs.Debugf(w.o, "multipart upload: wrote chunk %d with %v bytes", ossPartNumber, currentChunkSize)
 	return currentChunkSize, err
@@ -294,14 +317,18 @@ func (w *ossChunkWriter) WriteChunk(ctx context.Context, chunkNumber int, reader
 func (w *ossChunkWriter) Abort(ctx context.Context) (err error) {
 	// Abort the upload session
 	err = w.f.pacer.Call(func() (bool, error) {
-		err = w.bucket.AbortMultipartUpload(w.imur)
+		_, err = w.client.AbortMultipartUpload(ctx, &oss.AbortMultipartUploadRequest{
+			Bucket:   w.imur.Bucket,
+			Key:      w.imur.Key,
+			UploadId: w.imur.UploadId,
+		})
 		return w.shouldRetry(ctx, err)
 	})
 	if err != nil {
-		return fmt.Errorf("failed to abort multipart upload %q: %w", w.imur.UploadID, err)
+		return fmt.Errorf("failed to abort multipart upload %q: %w", *w.imur.UploadId, err)
 	}
-	w.shutdownRenew()
-	fs.Debugf(w.o, "multipart upload: %q aborted", w.imur.UploadID)
+	// w.shutdownRenew()
+	fs.Debugf(w.o, "multipart upload: %q aborted", *w.imur.UploadId)
 	return
 }
 
@@ -309,83 +336,21 @@ func (w *ossChunkWriter) Abort(ctx context.Context) (err error) {
 func (w *ossChunkWriter) Close(ctx context.Context) (err error) {
 	// Finalise the upload session
 	err = w.f.pacer.Call(func() (bool, error) {
-		_, err := w.bucket.CompleteMultipartUpload(w.imur, w.uploadedParts, w.callback...)
+		_, err := w.client.CompleteMultipartUpload(ctx, &oss.CompleteMultipartUploadRequest{
+			Bucket:   w.imur.Bucket,
+			Key:      w.imur.Key,
+			UploadId: w.imur.UploadId,
+			CompleteMultipartUpload: &oss.CompleteMultipartUpload{
+				Parts: w.uploadedParts,
+			},
+			Callback:    oss.Ptr(w.callback),
+			CallbackVar: oss.Ptr(w.callbackVar),
+		})
 		return w.shouldRetry(ctx, err)
 	})
 	if err != nil {
 		return fmt.Errorf("failed to complete multipart upload: %w", err)
 	}
-	w.shutdownRenew()
-	fs.Debugf(w.o, "multipart upload: %q finished", w.imur.UploadID)
+	fs.Debugf(w.o, "multipart upload: %q finished", *w.imur.UploadId)
 	return
-}
-
-// ------------------------------------------------------------
-
-// OnExpiry returns a channel which has the time written to it when
-// the token expires.  Note that there is only one channel so if
-// attaching multiple go routines it will only signal to one of them.
-func (w *ossChunkWriter) OnExpiry() <-chan time.Time {
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	if w.expiryTimer == nil {
-		w.expiryTimer = time.NewTimer(w.token.TimeToExpiry())
-	}
-	return w.expiryTimer.C
-}
-
-func (w *ossChunkWriter) renewToken(ctx context.Context) error {
-	token, err := w.f.getOSSToken(ctx)
-	if err != nil {
-		fs.Errorf(w.o, "failed to update OSS token: %v", err)
-		return err
-	}
-	w.mu.Lock()
-	defer w.mu.Unlock()
-	w.bucket.Client.Config.SecurityToken = token.SecurityToken
-	w.bucket.Client.Config.AccessKeyID = token.AccessKeyID
-	w.bucket.Client.Config.AccessKeySecret = token.AccessKeySecret
-	w.token = token
-	// Bump on the expiry timer if it is set
-	if w.expiryTimer != nil {
-		w.expiryTimer.Reset(w.token.TimeToExpiry())
-	}
-	return nil
-}
-
-func (w *ossChunkWriter) renewOnExpiry() {
-	expiry := w.OnExpiry()
-	for {
-		select {
-		case <-expiry:
-		case <-w.done:
-			return
-		}
-		fs.Debugf(w.o, "multipart upload: refreshing token...")
-		// Do a transaction
-		err := w.renewToken(context.Background())
-		if err == nil {
-			fs.Debugf(w.o, "multipart upload: token refresh successful")
-		} else {
-			fs.Errorf(w.o, "multipart upload: token refresh failed: %v", err)
-		}
-	}
-}
-
-// startRenew starts the token renewer
-func (w *ossChunkWriter) startRenew() {
-	w.done = make(chan any)
-	go w.renewOnExpiry()
-}
-
-// shutdownRenew stops the timer and no more renewal will take place.
-func (w *ossChunkWriter) shutdownRenew() {
-	if w == nil {
-		return
-	}
-	// closing a channel can only be done once
-	w.shutdown.Do(func() {
-		w.expiryTimer.Stop()
-		close(w.done)
-	})
 }

go.mod
@@ -15,7 +15,7 @@ require (
 	github.com/aalpar/deheap v0.0.0-20210914013432-0cc84d79dec3
 	github.com/abbot/go-http-auth v0.4.0
 	github.com/aead/ecdh v0.2.0
-	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible
+	github.com/aliyun/alibabacloud-oss-go-sdk-v2 v1.1.1
 	github.com/anacrolix/dms v1.7.1
 	github.com/anacrolix/log v0.15.2
 	github.com/andreburgaud/crypt2go v1.4.2

go.sum
@@ -93,8 +93,8 @@ github.com/aead/ecdh v0.2.0 h1:pYop54xVaq/CEREFEcukHRZfTdjiWvYIsZDXXrBapQQ=
 github.com/aead/ecdh v0.2.0/go.mod h1:a9HHtXuSo8J1Js1MwLQx2mBhkXMT6YwUmVVEY4tTB8U=
 github.com/akavel/rsrc v0.10.2 h1:Zxm8V5eI1hW4gGaYsJQUhxpjkENuG91ki8B4zCrvEsw=
 github.com/akavel/rsrc v0.10.2/go.mod h1:uLoCtb9J+EyAqh+26kdrTgmzRBFPGOolLWKpdxkKq+c=
-github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible h1:8psS8a+wKfiLt1iVDX79F7Y6wUM49Lcha2FMXt4UM8g=
-github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible/go.mod h1:T/Aws4fEfogEE9v+HPhhw+CntffsBHJ8nXQCwKr0/g8=
+github.com/aliyun/alibabacloud-oss-go-sdk-v2 v1.1.1 h1:Sc2T9vs8SCq0DErL/QIY3ZVx8F+dm+DIv4RFP9NLwC4=
+github.com/aliyun/alibabacloud-oss-go-sdk-v2 v1.1.1/go.mod h1:FTzydeQVmR24FI0D6XWUOMKckjXehM/jgMn1xC+DA9M=
 github.com/anacrolix/dms v1.7.1 h1:XVOpT3eoO5Ds34B1X+TE3R2ApfqGGeqotEoCVNP8BaI=
 github.com/anacrolix/dms v1.7.1/go.mod h1:excFJW5MKBhn5yt5ZMyeE9iFVqnO6tEGQl7YG/2tUoQ=
 github.com/anacrolix/generics v0.0.1 h1:4WVhK6iLb3UAAAQP6I3uYlMOHcp9FqJC9j4n81Wv9Ks=

commit 42e89a4aee5c3188ed5316bcebfe190bae425ad3

115: use `CredentialsFetcher` for persistent OSS token
Fixes potential token errors introduced in fb6c6553299cc5730a8f631760e4af966fc3c09a

backend/115/upload.go
@@ -248,7 +248,7 @@ func (f *Fs) getOSSToken(ctx context.Context) (info *api.OSSToken, err error) {
 }
 
 func (f *Fs) newOSSClient() (client *oss.Client) {
-	customerProvider := credentials.CredentialsProviderFunc(func(ctx context.Context) (credentials.Credentials, error) {
+	fetcher := credentials.CredentialsFetcherFunc(func(ctx context.Context) (credentials.Credentials, error) {
 		t, err := f.getOSSToken(ctx)
 		if err != nil {
 			return credentials.Credentials{}, err
@@ -259,9 +259,7 @@ func (f *Fs) newOSSClient() (client *oss.Client) {
 			SecurityToken:   t.SecurityToken,
 			Expires:         &t.Expiration}, nil
 	})
-	provider := credentials.CredentialsProviderFunc(func(ctx context.Context) (credentials.Credentials, error) {
-		return customerProvider.GetCredentials(ctx)
-	})
+	provider := credentials.NewCredentialsFetcherProvider(fetcher)
 
 	cfg := oss.LoadDefaultConfig().
 		WithCredentialsProvider(provider).