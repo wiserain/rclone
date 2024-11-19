wiserain/rclone

commit 399dfb4c3c53560fbdbff435e658ed3d90c4999d

115: fix the issue where the dircache was unable to correctly resolve rootParentID

backend/115/dircache/dircache.go
@@ -226,10 +226,10 @@ func (dc *DirCache) _findDir(ctx context.Context, path string, create bool) (pat
 		return pathID, nil
 	}
 
-	// mod - only when the Fs is shared
+	// mod - only when the Fs is not shared
 	if dc.trueRootID != "" {
 		dirPath := path
-		if dc.foundRoot {
+		if dc.foundRoot && dc.rootParentID != "" {
 			dirPath = dc.root + "/" + dirPath
 		}
 		pathID, err = dc.fs.GetDirID(ctx, dirPath)
@@ -254,7 +254,7 @@ func (dc *DirCache) _findDir(ctx context.Context, path string, create bool) (pat
 
 	}
 
-	// mod - only when the Fs is shared
+	// mod - only when the Fs is not shared
 	if dc.trueRootID != "" {
 		pathID, err = dc.fs.CreateDir(ctx, parentPathID, leaf)
 		if err != nil {
@@ -399,7 +399,7 @@ func (dc *DirCache) RootID(ctx context.Context, create bool) (ID string, err err
 func (dc *DirCache) RootParentID(ctx context.Context, create bool) (ID string, err error) {
 	dc.mu.Lock()
 	defer dc.mu.Unlock()
-	if !dc.foundRoot {
+	if dc.rootParentID == "" {
 		if dc.root == "" {
 			return "", errors.New("is root directory")
 		}

commit 10f9c0c5b2bc333ebee39edab2a73c584f5aee75

115: fix regression introduced in the previous commit
399dfb4c3c53560fbdbff435e658ed3d90c4999d

backend/115/dircache/dircache.go
@@ -364,7 +364,9 @@ func (dc *DirCache) _findRoot(ctx context.Context, create bool) error {
 	// Find the parent of the root while we still have the root
 	// directory tree cached
 	rootParentPath, _ := SplitPath(dc.root)
-	dc.rootParentID, _ = dc.Get(rootParentPath)
+	if rootParentID, ok := dc.Get(rootParentPath); ok {
+		dc.rootParentID = rootParentID
+	}
 
 	// Reset the tree based on dc.root
 	dc.Flush()
@@ -399,7 +401,7 @@ func (dc *DirCache) RootID(ctx context.Context, create bool) (ID string, err err
 func (dc *DirCache) RootParentID(ctx context.Context, create bool) (ID string, err error) {
 	dc.mu.Lock()
 	defer dc.mu.Unlock()
-	if dc.rootParentID == "" {
+	if !dc.foundRoot || dc.rootParentID == "" {
 		if dc.root == "" {
 			return "", errors.New("is root directory")
 		}