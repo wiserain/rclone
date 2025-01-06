wiserain/rclone

commit 6be84257249c37dc460361d816c76f9736791a1a

115: integrated processing of share link

backend/115/115.go
@@ -93,6 +93,14 @@ func init() {
 			Required:  true,
 			Sensitive: true,
 		}, {
+			Name:      "share_code",
+			Help:      "share code from share link",
+			Sensitive: true,
+		}, {
+			Name:      "receive_code",
+			Help:      "password from share link",
+			Sensitive: true,
+		}, {
 			// this is useless at the moment because there's no way to upload
 			// without defaultUserAgent and appVer 2.0.3.6
 			Name:     "user_agent",
@@ -222,6 +230,8 @@ type Options struct {
 	CID                 string               `config:"cid"`
 	SEID                string               `config:"seid"`
 	Cookie              string               `config:"cookie"`
+	ShareCode           string               `config:"share_code"`
+	ReceiveCode         string               `config:"receive_code"`
 	UserAgent           string               `config:"user_agent"`
 	RootFolderID        string               `config:"root_folder_id"`
 	ListChunk           int                  `config:"list_chunk"`
@@ -250,6 +260,7 @@ type Fs struct {
 	rootFolderID string
 	userID       string     // for uploads, adding offline tasks, and receiving from share link
 	userkey      string     // only for uploads
+	isShare      bool       // mark it is from shared or not
 	fileObj      *fs.Object // mod
 }
 
@@ -434,7 +445,7 @@ func newFs(ctx context.Context, name, path string, m configmap.Mapper) (*Fs, err
 	}
 
 	// mod - override rootID from path remote:{ID}
-	if rootID, _ := parseRootID(path); len(rootID) > 6 {
+	if rootID, _, _ := parseRootID(path); rootID != "" {
 		name += rootID
 		path = path[strings.Index(path, "}")+1:]
 	}
@@ -450,6 +461,7 @@ func newFs(ctx context.Context, name, path string, m configmap.Mapper) (*Fs, err
 		DuplicateFiles:          false, // duplicatefiles are only possible via web
 		CanHaveEmptyDirectories: true,  // can have empty directories
 		NoMultiThreading:        true,  // set if can't have multiplethreads on one download open
+		ServerSideAcrossConfigs: true,  // Can copy from shared FS (this is checked in Copy/Move/DirMove)
 	}).Fill(ctx, f)
 
 	if err := f.newClientWithPacer(ctx, opt); err != nil {
@@ -461,7 +473,7 @@ func newFs(ctx context.Context, name, path string, m configmap.Mapper) (*Fs, err
 
 // wraps dirCache.FindRoot() with warm-up cache
 func (f *Fs) dirCacheFindRoot(ctx context.Context) (err error) {
-	if f.rootFolderID != "0" {
+	if f.rootFolderID != "0" || f.isShare {
 		return f.dirCache.FindRoot(ctx, false)
 	}
 	for dir, dirID := f.root, "-1"; dirID != f.rootFolderID && dir != ""; {
@@ -484,7 +496,7 @@ func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, e
 
 	// mod - parse object id from path remote:{ID}
 	var srcFile *api.File
-	if rootID, _ := parseRootID(root); len(rootID) > 6 {
+	if rootID, receiveCode, _ := parseRootID(root); len(rootID) == 19 {
 		srcFile, err = f.getFile(ctx, rootID)
 		if err != nil {
 			return nil, err
@@ -496,7 +508,13 @@ func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, e
 			fs.Debugf(nil, "Root ID (Folder): %s", rootID)
 			srcFile = nil
 		}
+	} else if len(rootID) == 11 {
+		f.opt.ShareCode = rootID
+		f.opt.ReceiveCode = receiveCode
 	}
+
+	// mark it is from shared or not
+	f.isShare = f.opt.ShareCode != "" && f.opt.ReceiveCode != ""
 
 	// Set the root folder ID
 	if f.opt.RootFolderID != "" {
@@ -660,6 +678,9 @@ func (f *Fs) List(ctx context.Context, dir string) (entries fs.DirEntries, err e
 
 // CreateDir makes a directory with pathID as parent and name leaf
 func (f *Fs) CreateDir(ctx context.Context, pathID, leaf string) (newID string, err error) {
+	if f.isShare {
+		return "", errors.New("unsupported for shared filesystem")
+	}
 	info, err := f.makeDir(ctx, pathID, leaf)
 	if err != nil {
 		return
@@ -729,6 +750,9 @@ func (f *Fs) PutUnchecked(ctx context.Context, in io.Reader, src fs.ObjectInfo, 
 // MergeDirs merges the contents of all the directories passed
 // in into the first one and rmdirs the other directories.
 func (f *Fs) MergeDirs(ctx context.Context, dirs []fs.Directory) (err error) {
+	if f.isShare {
+		return errors.New("unsupported for shared filesystem")
+	}
 	if len(dirs) < 2 {
 		return nil
 	}
@@ -794,6 +818,10 @@ func (f *Fs) Mkdir(ctx context.Context, dir string) error {
 //
 // If it isn't possible then return fs.ErrorCantMove
 func (f *Fs) Move(ctx context.Context, src fs.Object, remote string) (fs.Object, error) {
+	if f.isShare {
+		fs.Debugf(src, "Can't move - shared filesystem")
+		return nil, fs.ErrorCantMove
+	}
 	if src.Fs().Name() != f.Name() {
 		return nil, fs.ErrorCantMove
 	}
@@ -852,6 +880,10 @@ func (f *Fs) Move(ctx context.Context, src fs.Object, remote string) (fs.Object,
 //
 // If destination exists then return fs.ErrorDirExists
 func (f *Fs) DirMove(ctx context.Context, src fs.Fs, srcRemote, dstRemote string) error {
+	if f.isShare {
+		fs.Debugf(src, "Can't move directory - shared filesystem")
+		return fs.ErrorCantDirMove
+	}
 	if src.Name() != f.Name() {
 		return fs.ErrorCantDirMove
 	}
@@ -897,6 +929,10 @@ func (f *Fs) DirMove(ctx context.Context, src fs.Fs, srcRemote, dstRemote string
 //
 // If it isn't possible then return fs.ErrorCantCopy
 func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object, error) {
+	if f.isShare {
+		fs.Debugf(src, "Can't copy - shared filesystem")
+		return nil, fs.ErrorCantCopy
+	}
 	srcObj, ok := src.(*Object)
 	if !ok {
 		fs.Debugf(src, "Can't copy - not same remote type")
@@ -918,8 +954,14 @@ func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object,
 		return nil, fs.ErrorCantCopy
 	}
 	// Copy the object
-	if err := f.copyFiles(ctx, []string{srcObj.id}, dstParentID); err != nil {
-		return nil, fmt.Errorf("couldn't copy file: %w", err)
+	if srcObj.fs.isShare {
+		if err := f.copyFromShareSrc(ctx, src, dstParentID); err != nil {
+			return nil, fmt.Errorf("couldn't copy from share: %w", err)
+		}
+	} else {
+		if err := f.copyFiles(ctx, []string{srcObj.id}, dstParentID); err != nil {
+			return nil, fmt.Errorf("couldn't copy file: %w", err)
+		}
 	}
 	// Update the copied object with new parent but old name
 	if info, err := dstObj.fs.readMetaDataForPath(ctx, srcObj.remote); err != nil {
@@ -948,6 +990,9 @@ func (f *Fs) Copy(ctx context.Context, src fs.Object, remote string) (fs.Object,
 // purgeCheck removes the root directory, if check is set then it
 // refuses to do so if it has anything in
 func (f *Fs) purgeCheck(ctx context.Context, dir string, check bool) error {
+	if f.isShare {
+		return errors.New("unsupported for shared filesystem")
+	}
 	root := path.Join(f.root, dir)
 	if root == "" {
 		return errors.New("can't purge root directory")
@@ -1141,6 +1186,18 @@ Usage:
 The 'path' should point to a directory not a file. Use an extra argument
 'subpath' to get an ID of a file located in '115:path'.
 `,
+}, {
+	Name:  "addshare",
+	Short: "Add shared files/dirs from a share link",
+	Long: `This command adds shared files/dirs from a share link.
+
+Usage:
+
+    rclone backend addshare 115:dirpath link
+
+All content from the link will be copied to the folder "dirpath". 
+If the path doesn't exist, rclone will create it for you.
+`,
 }}
 
 // Command the backend to run a named command
@@ -1153,6 +1210,9 @@ The 'path' should point to a directory not a file. Use an extra argument
 // If it is a string or a []string it will be shown to the user
 // otherwise it will be JSON encoded and shown to the user like that
 func (f *Fs) Command(ctx context.Context, name string, arg []string, opt map[string]string) (out interface{}, err error) {
+	if f.isShare {
+		return nil, errors.New("unsupported for shared filesystem")
+	}
 	switch name {
 	case "addurls":
 		return f.addURLs(ctx, "", arg)
@@ -1163,6 +1223,19 @@ func (f *Fs) Command(ctx context.Context, name string, arg []string, opt map[str
 			path = arg[0]
 		}
 		return f.getID(ctx, path)
+	case "addshare":
+		if len(arg) < 1 {
+			return nil, errors.New("need at least 1 argument")
+		}
+		shareCode, receiveCode, err := parseShareLink(arg[0])
+		if err != nil {
+			return nil, err
+		}
+		dirID, err := f.dirCache.FindDir(ctx, "", true)
+		if err != nil {
+			return nil, err
+		}
+		return nil, f.copyFromShare(ctx, shareCode, receiveCode, "", dirID)
 	case "stats": // 显示属性
 		path := ""
 		if len(arg) > 0 {
@@ -1354,7 +1427,11 @@ func (o *Object) setDownloadURL(ctx context.Context) (err error) {
 		return
 	}
 
-	o.durl, err = o.fs.getDownloadURL(ctx, o.pickCode)
+	if o.fs.isShare {
+		o.durl, err = o.fs.getDownloadURLFromShare(ctx, o.id)
+	} else {
+		o.durl, err = o.fs.getDownloadURL(ctx, o.pickCode)
+	}
 	return
 }
 

backend/115/api/types.go
@@ -374,3 +374,51 @@ type NewURL struct {
 	} `json:"result,omitempty"`
 	Errcode int `json:"errcode,omitempty"`
 }
+
+type ShareSnap struct {
+	State bool           `json:"state,omitempty"`
+	Error string         `json:"error,omitempty"`
+	Errno Int            `json:"errno,omitempty"`
+	Data  *ShareSnapData `json:"data,omitempty"`
+}
+
+type ShareSnapData struct {
+	Userinfo struct {
+		UserID   string `json:"user_id,omitempty"`
+		UserName string `json:"user_name,omitempty"`
+		Face     string `json:"face,omitempty"`
+	} `json:"userinfo,omitempty"`
+	Shareinfo struct {
+		SnapID           string `json:"snap_id,omitempty"`
+		FileSize         string `json:"file_size,omitempty"`
+		ShareTitle       string `json:"share_title,omitempty"`
+		ShareState       string `json:"share_state,omitempty"`
+		ForbidReason     string `json:"forbid_reason,omitempty"`
+		CreateTime       string `json:"create_time,omitempty"`
+		ReceiveCode      string `json:"receive_code,omitempty"`
+		ReceiveCount     string `json:"receive_count,omitempty"`
+		ExpireTime       int    `json:"expire_time,omitempty"`
+		FileCategory     int    `json:"file_category,omitempty"`
+		AutoRenewal      string `json:"auto_renewal,omitempty"`
+		AutoFillRecvcode string `json:"auto_fill_recvcode,omitempty"`
+		CanReport        int    `json:"can_report,omitempty"`
+		CanNotice        int    `json:"can_notice,omitempty"`
+		HaveVioFile      int    `json:"have_vio_file,omitempty"`
+	} `json:"shareinfo,omitempty"`
+	Count      int     `json:"count,omitempty"`
+	List       []*File `json:"list,omitempty"`
+	ShareState string  `json:"share_state,omitempty"`
+	UserAppeal struct {
+		CanAppeal       int `json:"can_appeal,omitempty"`
+		CanShareAppeal  int `json:"can_share_appeal,omitempty"`
+		PopupAppealPage int `json:"popup_appeal_page,omitempty"`
+		CanGlobalAppeal int `json:"can_global_appeal,omitempty"`
+	} `json:"user_appeal,omitempty"`
+}
+
+type ShareDownloadInfo struct {
+	FileID   string      `json:"fid"`
+	FileName string      `json:"fn"`
+	FileSize Int64       `json:"fs"`
+	URL      DownloadURL `json:"url"`
+}

backend/115/helper.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/rclone/rclone/backend/115/api"
@@ -29,6 +30,9 @@ type listAllFn func(*api.File) bool
 //
 // If the user fn ever returns true then it early exits with found = true
 func (f *Fs) listAll(ctx context.Context, dirID string, fn listAllFn) (found bool, err error) {
+	if f.isShare {
+		return f.listShare(ctx, dirID, fn)
+	}
 	// Url Parameters
 	params := url.Values{}
 	params.Set("aid", "1")
@@ -238,11 +242,15 @@ func (f *Fs) indexInfo(ctx context.Context) (data *api.IndexInfo, err error) {
 }
 
 func (f *Fs) _getDownloadURL(ctx context.Context, input []byte) (output []byte, cookies []*http.Cookie, err error) {
+	rootURL := "https://proapi.115.com/app/chrome/downurl"
+	if f.isShare {
+		rootURL = "https://proapi.115.com/app/share/downurl"
+	}
 	key := crypto.GenerateKey()
 	t := strconv.Itoa(int(time.Now().Unix()))
 	opts := rest.Opts{
 		Method:          "POST",
-		RootURL:         "https://proapi.115.com/app/chrome/downurl",
+		RootURL:         rootURL,
 		Parameters:      url.Values{"t": {t}},
 		MultipartParams: url.Values{"data": {crypto.Encode(input, key)}},
 	}
@@ -431,5 +439,137 @@ func (f *Fs) addURLs(ctx context.Context, dir string, urls []string) (info *api.
 	if err = json.Unmarshal(output, &info); err != nil {
 		return nil, fmt.Errorf("failed to json.Unmarshal %q", string(output))
 	}
+	return
+}
+
+// ------------------------------------------------------------
+
+// parses arguments for Shared from following URL pattern
+//
+// https://115.com/s/{shareCode}?password={receiveCode}
+func parseShareLink(rawURL string) (shareCode, receiveCode string, err error) {
+	if !strings.HasPrefix(rawURL, "http") || !strings.Contains(rawURL, "/s/") {
+		return "", "", fmt.Errorf("%q is not a share link", rawURL)
+	}
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		return "", "", fmt.Errorf("invalid share link format: %w", err)
+	}
+	q, err := url.ParseQuery(u.RawQuery)
+	if err != nil {
+		return "", "", fmt.Errorf("invalid share link format: %w", err)
+	}
+	return strings.TrimPrefix(u.Path, "/s/"), q.Get("password"), nil
+}
+
+// listing filesystem from share link
+//
+// no need user authorization by cookies
+func (f *Fs) listShare(ctx context.Context, dirID string, fn listAllFn) (found bool, err error) {
+	// Url Parameters
+	params := url.Values{}
+	params.Set("share_code", f.opt.ShareCode)
+	params.Set("receive_code", f.opt.ReceiveCode)
+	params.Set("cid", dirID)
+	params.Set("limit", strconv.Itoa(f.opt.ListChunk))
+
+	opts := rest.Opts{
+		Method:     "GET",
+		Path:       "/share/snap",
+		Parameters: params,
+	}
+
+	offset := 0
+OUTER:
+	for {
+		params.Set("offset", strconv.Itoa(offset))
+
+		var info api.ShareSnap
+		var resp *http.Response
+		err = f.pacer.Call(func() (bool, error) {
+			resp, err = f.srv.CallJSON(ctx, &opts, nil, &info)
+			return shouldRetry(ctx, resp, &info, err)
+		})
+		if err != nil {
+			return found, fmt.Errorf("couldn't list files: %w", err)
+		} else if !info.State {
+			return found, fmt.Errorf("API State false: %q (%d)", info.Error, info.Errno)
+		}
+		if len(info.Data.List) == 0 {
+			break
+		}
+		for _, item := range info.Data.List {
+			item.Name = f.opt.Enc.ToStandardName(item.Name)
+			if ts, terr := strconv.ParseInt(item.T, 10, 64); terr == nil {
+				item.Te = api.Time(time.Unix(ts, 0))
+			}
+			if fn(item) {
+				found = true
+				break OUTER
+			}
+		}
+		offset += f.opt.ListChunk
+		if offset >= info.Data.Count {
+			break
+		}
+	}
+	return
+}
+
+// copyFromShare copies shared object by its shareCode, receiveCode, fid
+//
+// fid = "0" or "" means root directory containing all files/dirs
+func (f *Fs) copyFromShare(ctx context.Context, shareCode, receiveCode, fid, cid string) (err error) {
+	form := url.Values{}
+	form.Set("share_code", shareCode)     // src
+	form.Set("receive_code", receiveCode) // src
+	form.Set("file_id", fid)              // src
+	form.Set("cid", cid)                  // dst
+	form.Set("user_id", f.userID)         // dst
+
+	opts := rest.Opts{
+		Method:          "POST",
+		Path:            "/share/receive",
+		MultipartParams: form,
+	}
+
+	var info *api.Base
+	var resp *http.Response
+	err = f.pacer.Call(func() (bool, error) {
+		resp, err = f.srv.CallJSON(ctx, &opts, nil, &info)
+		return shouldRetry(ctx, resp, info, err)
+	})
+	if err != nil {
+		return
+	} else if !info.State {
+		return fmt.Errorf("API State false: %s (%d)", info.Error, info.Errno)
+	}
+	return
+}
+
+func (f *Fs) copyFromShareSrc(ctx context.Context, src fs.Object, cid string) (err error) {
+	srcObj, _ := src.(*Object) // this is already checked
+	return f.copyFromShare(ctx, srcObj.fs.opt.ShareCode, srcObj.fs.opt.ReceiveCode, srcObj.id, cid)
+}
+
+func (f *Fs) getDownloadURLFromShare(ctx context.Context, fid string) (durl *api.DownloadURL, err error) {
+	// file_id -> data -> reqData
+	input, _ := json.Marshal(map[string]string{
+		"share_code":   f.opt.ShareCode,
+		"receive_code": f.opt.ReceiveCode,
+		"file_id":      fid,
+	})
+	output, cookies, err := f._getDownloadURL(ctx, input)
+	if err != nil {
+		return
+	}
+	downInfo := api.ShareDownloadInfo{}
+	if err := json.Unmarshal(output, &downInfo); err != nil {
+		return nil, fmt.Errorf("failed to json.Unmarshal %q", string(output))
+	}
+
+	durl = &downInfo.URL
+	durl.Cookies = cookies
+	durl.CreateTime = time.Now()
 	return
 }

backend/115/mod.go
@@ -12,19 +12,27 @@ import (
 // ------------------------------------------------------------
 
 // parseRootID parses RootID from path
-func parseRootID(s string) (rootID string, err error) {
-	re := regexp.MustCompile(`^\{(\d{19})\}`)
+func parseRootID(s string) (rootID, receiveCode string, err error) {
+	re := regexp.MustCompile(`^\{([^}]{11,})\}`)
 	m := re.FindStringSubmatch(s)
 	if m == nil {
-		return "", fmt.Errorf("%s does not contain a valid id", s)
+		return "", "", fmt.Errorf("%s does not contain a valid id", s)
 	}
 	rootID = m[1]
-	return
+
+	re = regexp.MustCompile(`(cid=)?(\d{19})`)
+	if m := re.FindStringSubmatch(rootID); m != nil {
+		// https://115.com/?cid={CID}&offset=0&tab=&mode=wangpan
+		// {CID}
+		return m[len(m)-1], "", nil
+	}
+	// Assume it is a share link
+	return parseShareLink(rootID)
 }
 
 // get an id of file or directory
 func (f *Fs) getID(ctx context.Context, path string) (id string, err error) {
-	if id, _ := parseRootID(path); len(id) > 6 {
+	if id, _, _ := parseRootID(path); len(id) == 19 {
 		info, err := f.getFile(ctx, id)
 		if err != nil {
 			return "", fmt.Errorf("no such object with id %q: %w", id, err)

backend/115/upload.go
@@ -308,6 +308,9 @@ func calcBlockSHA1(ctx context.Context, in io.Reader, src fs.ObjectInfo, rangeSp
 
 // upload uploads the object with or without using a temporary file name
 func (f *Fs) upload(ctx context.Context, in io.Reader, src fs.ObjectInfo, remote string, options ...fs.OpenOption) (fs.Object, error) {
+	if f.isShare {
+		return nil, errors.New("unsupported for shared filesystem")
+	}
 	size := src.Size()
 
 	// check upload available

commit 915f4c11528bd43217251c5584a9b9ce9ecfc142

115: fix cross-config copy failure
This regression was introduced in 6be84257249c37dc460361d816c76f9736791a1a

backend/115/115.go
@@ -627,7 +627,6 @@ func newFs(ctx context.Context, name, path string, m configmap.Mapper) (*Fs, err
 		DuplicateFiles:          false, // duplicatefiles are only possible via web
 		CanHaveEmptyDirectories: true,  // can have empty directories
 		NoMultiThreading:        true,  // set if can't have multiplethreads on one download open
-		ServerSideAcrossConfigs: true,  // Can copy from shared FS (this is checked in Copy/Move/DirMove)
 	}).Fill(ctx, f)
 
 	// setting appVer
@@ -690,6 +689,9 @@ func NewFs(ctx context.Context, name, root string, m configmap.Mapper) (fs.Fs, e
 	} else {
 		f.rootFolderID = "0" //根目录 = root directory
 	}
+
+	// Can copy from shared FS (this is checked in Copy/Move/DirMove)
+	f.features.ServerSideAcrossConfigs = f.isShare
 
 	// Set the root folder path if it is not on the absolute root
 	if f.rootFolderID != "" && f.rootFolderID != "0" {